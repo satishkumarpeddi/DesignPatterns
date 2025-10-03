d02c94157eec2210c7bd81fb93ef0c69f1f9b83c
evhook <anonymous1986222@gmail.com>
2025-10-03 11:31:03 +0530

    Thread Safe 🚀🚀

---
+++ SingleTonPattern/DbSingleton.java
@@ -0,0 +1,11 @@
+package SingleTonPattern;
+public class DbSingleton {
+   
+    private static class LazyHolder{
+        static final DbSingleton INSTANCE = new DbSingleton();
+    }
+    private DbSingleton(){}
+    public static DbSingleton getInstance(){
+        return LazyHolder.INSTANCE;
+    }
+}
